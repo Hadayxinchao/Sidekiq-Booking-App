bf40ad38d1158116f8066088628d8fd9fdb85fb4
Hoang Ha <SmithMerch2301@gmail.com>
2024-10-01 17:00:56 +0700

    Handle Failures by Planning Ahead

--- sidekiq-book/config/initializers/error_catcher.rb
+++ sidekiq-book/config/initializers/error_catcher.rb
@@ -1,3 +1,3 @@
 Rails.application.config.to_prepare do
-  # ErrorCatcherServiceWrapper.ignored_errors << error class here
+  ErrorCatcherServiceWrapper.ignored_errors << IgnorableExceptionSinceSidekiqWillRetry
 end
---
+++ sidekiq-book/test/jobs/ignorable_exception_since_sidekiq_will_retry_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class IgnorableExceptionSinceSidekiqWillRetryTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
